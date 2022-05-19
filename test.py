7661505aaa7022d4d97caedcab9857e3b15258c6
tomiwa135 <96557665+tomiwa135@users.noreply.github.com>
2022-05-19 00:17:15 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,6 @@
+from unicode import name
 import yaml
 import json
-
-NAMESPACE = "${namespace}"
-DOMAIN_NAME_1 = "${Domain_1}"
-PORT_1 = "${Port_1}"
 
 with open(r"input.json") as json_file:
     json_data = json.load(json_file)
@@ -14,11 +11,14 @@ with open(r"networkpolicy.yaml") as yaml_file:
 for namespace_name in json_data:
     new_yaml_data = dict(yaml_data_template)
     current_namespace = json_data[namespace_name]
-    domain = current_namespace['domain']
-    port = current_namespace['port']
+    
+    domains = current_namespace[0].split("/")
+    ports = current_namespace[1].split("/")
+    
     new_yaml_data['metadata']['namespace']= namespace_name
-    new_yaml_data['spec']['egress'][0]['destination']['domains'][0] = domain 
-    new_yaml_data['spec']['egress'][0]['destination']['ports'][0] = port
-    new_yaml_data_text = yaml.dump(new_yaml_data)
+    new_yaml_data['spec']['egress']['destination']['domains']= domains 
+    new_yaml_data['spec']['egress']['destination']['ports'] = ports
+    
+    new_yaml_data_text = yaml.safe_dump(new_yaml_data, allow_unicode=True)
     
     print(new_yaml_data_text)
